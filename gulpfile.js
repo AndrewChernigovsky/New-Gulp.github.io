5a80dcaf060149bada9b743884da37887bd640b8
Andrew <chernigovsky108@gmail.com>
2021-09-06 21:45:16 +0300

    4

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,28 @@
-const gulp = require('gulp');
-const gulpPug = require('gulp-pug');
+const gulp = require("gulp");
+const gulpPug = require("gulp-pug");
+const gulpPlumber = require("gulp-plumber");
+const gulpSass = require("gulp-sass")(require("sass"));
 
 function pugToHtml() {
-    return gulp.src('source/pug/pages/*.pug')
-    .pipe(gulpPug())
-    .pipe(gulp.dest('build'));
+  return gulp
+    .src("source/pug/pages/*.pug")
+    .pipe(gulpPlumber())
+    .pipe(
+      gulpPug({
+        pretty: true,
+      })
+    )
+    .pipe(gulpPlumber.stop())
+    .pipe(gulp.dest("build"));
+}
+
+function scssToCss() {
+    return gulp
+      .src("source/static/styles/style.scss")
+      .pipe(gulpPlumber())
+      .pipe(gulpSass())
+      .pipe(gulpPlumber.stop())
+      .pipe(gulp.dest("build/static/css/"));
   }
-  
-  exports.default = gulp.series(pugToHtml);
+
+exports.default = gulp.series(pugToHtml, scssToCss);
