1732ac1600d0b74e6da0379ddf80b6091abd8b9b
Andrew <chernigovsky108@gmail.com>
2021-09-07 09:26:01 +0300

    5

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require("gulp");
 const gulpPug = require("gulp-pug");
 const gulpPlumber = require("gulp-plumber");
 const gulpSass = require("gulp-sass")(require("sass"));
+const gulpAutoprefixer = require("gulp-autoprefixer");
+const gulpCssMinify = require("gulp-clean-css");
+const sourcemaps = require('gulp-sourcemaps');
 
 function pugToHtml() {
   return gulp
@@ -21,6 +24,10 @@ function scssToCss() {
       .src("source/static/styles/style.scss")
       .pipe(gulpPlumber())
       .pipe(gulpSass())
+      .pipe(gulpAutoprefixer())
+      .pipe(gulpCssMinify())
+      .pipe(sourcemaps.init())
+      .pipe(sourcemaps.write('maps/'))
       .pipe(gulpPlumber.stop())
       .pipe(gulp.dest("build/static/css/"));
   }
